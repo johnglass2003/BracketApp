ba038abb31ad58eea6be39b4f79fbe3f593581fb
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2023-03-21 15:07:05 -0400

    innit

---
+++ BracketApp/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Member struct {
+	name string
+}
+
+type Match struct {
+	member1     Member
+	member2     Member
+	member1Wins uint
+	member2Wins uint
+}
+
+func main() {
+	fmt.Print("Type a number of rounds: ")
+	var rounds float64
+
+	fmt.Print("Type a number of rounds: ")
+	fmt.Scan(&rounds)
+	rounds = math.Pow(2, math.Ceil(math.Log(rounds)/math.Log(2)))
+
+	var r int = int(rounds)
+	var matches = make([]Match, r)
+
+	UNINITIALIZED_MEMBER := Member{name: "UNINITIALIZED"}
+	UNINITIALIZED_MATCH := Match{member1: UNINITIALIZED_MEMBER, member2: UNINITIALIZED_MEMBER, member1Wins: 0, member2Wins: 0}
+
+	for i := 0; i < r; i++ {
+		matches = append(matches, UNINITIALIZED_MATCH)
+	}
+
+	for i := 0; i < r; i++ {
+		fmt.Println(matches[i].member1.name + " " + matches[i].member2.name)
+	}
+
+	fmt.Println("Hello!")
+}
